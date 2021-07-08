8b1413b973199910009afa887945a252ad1233e0
Shubham Gautam <72097846+Shub-Gautam@users.noreply.github.com>
2021-07-08 20:31:44 +0530

    deployment final

---
+++ client/src/service/api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const URL = 'http://localhost:8000';
+
+export  const createPost = async (post)=>{
+    try{ return await axios.post( ''+URL+'/create',post )}
+    catch(error){
+        console.log('Error while calling createPost API',error);
+    }
+}
+
+export const getAllPosts = async ()=>{
+    try{
+       let response = await axios.get(`${URL}/posts`);
+       return response.data;
+    }catch(e){
+        console.log("Error while calling getAllPosts API",e);
+    }
+}
+
+export const getPost = async (id)=>{
+    try {
+        let res = await axios.get(`${URL}/post/${id}`);
+        return res.data;
+    }catch (e){
+        console.log("Error while calling getPost API",e);
+    }
+}
